4e4405ceae2e8af9e9257e0e7c8dc509276dc97f
Hamidou Diallo <dio.hamidou@gmail.com>
2019-09-23 20:08:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -109,7 +109,6 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for] = alans_new_info
-     programmer_hash[:alan_kay][:languages][1] = "Assembly"
      return programmer_hash
      
      
@@ -131,6 +130,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     
+     programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
   return programmer_hash
 end
