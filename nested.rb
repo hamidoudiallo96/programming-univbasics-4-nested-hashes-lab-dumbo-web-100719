5750cc18d081e8fac9123fe33606a57b214d3607
Hamidou Diallo <dio.hamidou@gmail.com>
2019-09-23 20:00:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+  return programmer_hash
 end
 
 def adding_matz
@@ -129,5 +130,5 @@ def adding_to_dennis
         }
      }
      
-
+  return programmer_hash
 end
